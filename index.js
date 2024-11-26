bc5d86052ece8aac4cd8456454c0343c6c3af023
avdbp <alejandro.vdb@gmail.com>
2024-11-25 22:17:35 -0500

    prueba

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const transporter = nodemailer.createTransport({
 app.post('/send-email', async (req, res) => {
     const { name, email, phone, message } = req.body;
 
-    if (!name || !email || !phone || !message) {
+    if (name || email || phone || message) {
         return res.status(400).json({ error: 'Todos los campos son obligatorios.' });
     }
 
